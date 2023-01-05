e8c935d15898a86f5efcddcd0a8703e853ed62ab
shota <sspydery0@gmail.com>
2023-01-05 15:28:05 +0900

    [add] add sending close_notify function.

--- src/SslTcpClient.cs
+++ src/SslTcpClient.cs
@@ -108,7 +108,7 @@ namespace tcp_client
             {
                 if(_sslStream != null)
                 {
-                    _sslStream.Close();
+                    SslDirectCall.CloseNotify(_sslStream);
                 }
 
                 if (_client != null)
